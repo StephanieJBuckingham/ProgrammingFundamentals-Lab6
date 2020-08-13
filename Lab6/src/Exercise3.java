cfb09b4fce24ae32a84a70d0c207e63e18dc05fb
StephanieJBuckingham <68093533+StephanieJBuckingham@users.noreply.github.com>
2020-08-13 14:02:05 -0400

    Modify Exercise 3

--- Lab6/src/Exercise3.java
+++ Lab6/src/Exercise3.java
@@ -9,15 +9,15 @@
 	*		 return result;
 	*/
 
-/*  To find the time complexity you need to add the time 
+/* To find the time complexity you need to add the time 
  * complexity for each line of the code. To determine the
- *  Big O Notation you need to pick the fastest growing term. 
+ * Big O Notation you need to pick the fastest growing term. 
  * 
  * line 4 - c1(constant)
  * line 5 - n/2 -1
  * line 7 - c2(constant)
  * line 9- c3(constant)
  * 
- * Time Complexity = c1 + (n/2 - 1) + c2 + c3
+ * Time Complexity = c1 + n/2 + c2 + c3
  * Big O Notation = O(n)
  */
